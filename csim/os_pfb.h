8a40f48c949e858d3c2df2663693f945cb1016f0
Mitch Burnett <mitch.burnett@byu.edu>
2019-04-05 14:31:20 -0600

    Definition and Header files for OS PFB

---
+++ csim/os_pfb.h
@@ -0,0 +1,8 @@
+#ifndef OS_PFB_H
+#define OS_PFB_H
+
+#include "typedefs.h"
+
+void os_pfb(datain_t *in_real, datain_t *in_imag, dataout_t *out_real, dataout *out_imag);
+
+#endif // OS_PFB_H
---
+++ csim/typedefs.h
@@ -0,0 +1,8 @@
+#ifndef TYPEDEFS_H
+#define TYPEDEFS_H
+
+typedef float coeff_t;
+typedef signed char datain_t;
+typedef float dataout_t;
+
+#endif // TYPEDEFS_H
