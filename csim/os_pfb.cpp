6160c5121ba94a320e36be964ae9b425ad2caef6
Mitch Burnett <mitch.burnett@byu.edu>
2019-04-16 23:25:30 -0600

    Initial pragma optimizations
    Made some quick optimizations for some results and comparisons. Will come back
    to this later and just want to capture for now.

--- csim/os_pfb.cpp
+++ csim/os_pfb.cpp
@@ -1,46 +1,34 @@
-#include <stdio.h>
-#include <iostream>
-
 #include "os_pfb.h"
 
-//void os_pfb(cx_datain_t in[M], cx_dataout_t out[M], int shift_states[SHIFT_STATES], bool* overflow)
-void os_pfb(cx_datain_t in[M], cx_dataout_t out[M], bool* overflow)
-{
-//#pragma HLS interface ap_fifo depth=FFT_LENGTH port=in,out
-//#pragma HLS interface ap_fifo depth=1 port=overflow
+void os_pfb(cx_datain_t in[D], cx_dataout_t out[M], bool* overflow) {
 #pragma HLS interface axis depth=FFT_LENGTH port=in,out
 #pragma HLS data_pack variable=in
 #pragma HLS data_pack variable=out
 #pragma HLS dataflow
-
   // filter taps
   const coeff_t h[L] = {
     #include "coeff.dat"
   };
-
-  // shift states that have been pre-determined. Need to figure out how to auto-generate
+#pragma HLS ARRAY_PARTITION variable=h complete dim=1
+  // shift states that have been pre-computed.
   static int shift_states[SHIFT_STATES] = {0, 24, 16, 8};
-
+#pragma HLS ARRAY_PARTITION variable=shift_states complete dim=1
+  // maintain filter state for subsequent calls
   static cx_dataout_t filter_state[L];
-
-  cx_dataout_t filter_out[M] = { 0 };
-
-  // Note: in the fir loop...
-  // interchange p and m iterators for m to be fastest moving for sequential memory access. This can be seen from
-  // the 'idx' variable because m is the fastest moving element and the fact p is multiplied by M shows that p controls
-  // row jumps (if it were a two dimensional data structure--which it is in the model/block diagram).
+#pragma HLS ARRAY_PARTITION variable=filter_state complete dim=1
 
   // shift/capture samples and polyphase fir filter
+  cx_dataout_t filter_out[M] = { 0 };
   for (int p = P-1; p >= 0 ; --p) {
+#pragma HLS unroll complete
     for (int m = M-1; m >= 0; --m) {
+#pragma HLS pipeline II=1
       int idx = p*M+m;
-
       if (idx <= D-1) {
         filter_state[idx] = in[idx];
       } else {
         filter_state[idx] = filter_state[idx-D];
       }
-
       filter_out[m] = filter_out[m] + h[idx]*filter_state[idx];
     }
   }
@@ -50,37 +38,29 @@ void os_pfb(cx_datain_t in[M], cx_dataout_t out[M], bool* overflow)
   int oidx;
   cx_dataout_t ifft_buffer[M] = { 0 };
   for (int i=0; i<M; ++i) {
+#pragma HLS unroll complete
     oidx = (i+shift) % M;
     ifft_buffer[oidx] = filter_out[i];
   }
 
-  // move shift array up by one and copy end to beginning
+  // circularly rotate states array
   int tmp = shift_states[SHIFT_STATES-1];
   for (int i=SHIFT_STATES-1; i > 0; --i) {
     shift_states[i] = shift_states[i-1];
   }
   shift_states[0] = tmp;
-
   //ifft computation
   os_pfb_config_t ifft_config;
   os_pfb_status_t ifft_status;
-
   ifft_config.setDir(0); // inverse transform
 
-//  hls::fft<os_pfb_config>(ifft_buffer, out, &ifft_status, &ifft_config);
   cx_dataout_t ifft_out[M] = { 0 };
   hls::fft<os_pfb_config>(ifft_buffer, ifft_out, &ifft_status, &ifft_config);
-
   *overflow = ifft_status.getOvflo() & 0x1;
 
-  // hls::fft implements the output as an ap_fifo (although the HLS documentation says it is a stream? Unless
-  // stream and ap_fifo are synonyms and so I had to add another variable to explicittly set as an axi stream.
-  // Also interesting that I had to start at i=0 and copy out to M. Couldn't do what I had been doing
-  // of starting at the end of the array. I wonder if this is has to do with how hls::fft fills ifft_out
+  // stream data out
   for (int i=0; i<M; ++i)
     out[i] = ifft_out[i];
-
-  return;
 }
 
 
--- csim/os_pfb.h
+++ csim/os_pfb.h
@@ -11,6 +11,7 @@
 
 #define SHIFT_STATES 4 // for the above D=24, M=32 there are 4 shifting states
 
+
 // Vivado FFT IP configuration
 const int FFT_LENGTH = M;
 
@@ -21,18 +22,13 @@ struct os_pfb_config : hls::ip_fft::params_t {
   static const unsigned output_width = 32;
   static const unsigned config_width = 8;
   static const unsigned phase_factor_width = 24;
-  // stages_block_ram does not update from inherited struct
   static const unsigned stages_block_ram = (max_nfft < 10) ? 0 : (max_nfft - 9);
-
-  // the example configures the FFT width, what else do I need here... time to
-  // check the user guide...
 };
 
 typedef hls::ip_fft::config_t<os_pfb_config> os_pfb_config_t;
 typedef hls::ip_fft::status_t<os_pfb_config> os_pfb_status_t;
 
-//void os_pfb(cx_datain_t in[M], cx_dataout_t out[M], int shift_states[SHIFT_STATES], bool* overflow);
-void os_pfb(cx_datain_t in[M], cx_dataout_t out[M], bool* overflow);
+void os_pfb(cx_datain_t in[D], cx_dataout_t out[M], bool* overflow);
 
 #endif // OS_PFB_H
 
