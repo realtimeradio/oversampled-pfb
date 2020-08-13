Fixed bug found when scaling to M=2048
State machine bug from adding the delay fifo caused timing to be off. The `vin`
assertion was not being asserted on the first data sample coming out of the
wait fifo state. Also was being set twice and not default value of zero at the
top of the state machine logic.

Tested values of M=64, 256, 512, 2048 with counter, impulse and full windowed
coefficients everythong seems to check out.

python/hw_utils/plt_xfft.py
@@ -15,6 +15,7 @@ if __name__=="__main__":
   parser.add_argument('nframes', type=int, help='Number of FFT frames of length nfft')
   parser.add_argument('-X', '--transform', action='store_true', help="perform FFT on input file")
   parser.add_argument('-p', '--plot', action='store_true', help="plot the data")
+  parser.add_argument('-l', '--linear', action='store_true', help="plot linear")
   args = parser.parse_args()
 
   # parase arguments
@@ -35,7 +36,10 @@ if __name__=="__main__":
   if (args.transform):
     X = fft(X, NFFT)
 
-  magX = 20*log10(abs(X+.0001))
+  if (args.linear):
+    magX = np.real(X)
+  else:
+    magX = 20*log10(abs(X+.0001))
 
   if (args.plot):
     PLT_WIDTH = 4