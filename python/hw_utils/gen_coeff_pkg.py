fix scaling, coeff generation, adc model packed
The adc model now generates data like the rfdc by MSB aligning the data (packed
left with zeros remaining in the AXI word). The adc model also now correctly
rounds and saturates within the specified adc bit range.

With MSB aligning the data the `xpm_pe` default scaling of selecting the top
`WIDTH` bits will always be the safe option but because the adc data output
widths are always some number of bits less than the AXI word `WIDTH` a more
optimal slicing may exist. Furthermore, the extra guard bit added on addition
may never be used an can be dropped.

Also, since the Xilinx DSP48E is an 18-bit input multiplier if I can show
that through the P number of multiply adds (in our case eight) of the polyphase
fir will never exceed 18 it makes sense that I will be able to just always be
able to select the top 18 bits and even slice within this range and just use the
extra 4 bits (since the adc is 14 bits) as overflow guard bits.

It would make sense though that we still want to round. In which case I still
need to come back and apply a rounding cycle in the multiply add module and
increase the latency accordingly.

The coeff generation file had a bug where it incorrectly scaled and was not
rounding or truncating the coefficients. It also always packed 16 bit
coefficients instead of packing into other specified widths. It was because of
these bugs that the fractional fixed point representation of the data had to
have a zero fractional binary point value.

python/hw_utils/gen_coeff_pkg.py
@@ -111,6 +111,7 @@ def gen_packed_coeff_pkg_file(h, pkg_info=alpaca_coeff_pkg):
   fp.write("\n\n\t};\n\n")
   fp.write("endpackage : {:s}".format(PKG_NAME))
   fp.close()
+
 if __name__=="__main__":
 
   parser = argparse.ArgumentParser()
@@ -131,11 +132,15 @@ if __name__=="__main__":
 
   # compute filter
   if (window == "hann"):
-    h = HannWin.genTaps(M, P, D)
-    filter_pk = np.max(h)
-    lsb_scale = filter_pk/(2**(BITS-1)-1)
-    h_scale = h/lsb_scale;
-    h = np.array(h_scale, dtype=np.int16)
+    h_win = HannWin.genTaps(M, P, D)
+    filter_pk = np.max(h_win)
+    h_norm = h_win/filter_pk
+    lsb_scale = 2**(-(BITS-1))
+    h_scale = h_norm/lsb_scale
+    h_sat = np.round(h_scale) 
+    h_sat = np.where(h_sat < -2**(BITS-1), -2**(BITS-1), h_sat)
+    h_sat = np.where(h_sat > 2**(BITS-1)-1, 2**(BITS-1)-1, h_sat)
+    h_quant = np.int16(h_sat)
   elif (window == "rect"):
     h = Ones.genTaps(M, P, D)
     filter_pk = np.max(h)
@@ -153,6 +158,7 @@ if __name__=="__main__":
   alpaca_coeff_pkg["name"] = "alpaca_ospfb_{:s}_{:d}_{:d}_coeff_pkg".format(window, M, P)
   alpaca_coeff_pkg["ptaps"] = P
   alpaca_coeff_pkg["ntaps"] = M
-  gen_packed_coeff_pkg_file(h, alpaca_coeff_pkg)
+  alpaca_coeff_pkg["width"] = BITS
+  gen_packed_coeff_pkg_file(h_quant, alpaca_coeff_pkg)
 
 